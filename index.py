eb3ad879643979c720526c9eaf1921455890ee4c
toyohito.sawada <nqdior@gmail.com>
2024-06-28 19:03:29 +0900

    set async

--- index.py
+++ index.py
@@ -1,36 +1,27 @@
-import cProfile
-import pstats
-import io
 import bottle
 from bottle import route, run, template
+import json
 import image
-from memory_profiler import profile
+import gevent
+from gevent import monkey
 
-@profile
+monkey.patch_all()
+
 def call_service():
     directoryName = 'photos'
     image.process(directoryName)
 
-def profile_service():
-    pr = cProfile.Profile()
-    pr.enable()
-    call_service()
-    pr.disable()
-
-    s = io.StringIO()
-    sortby = 'cumulative'
-    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-    ps.print_stats()
-    print(s.getvalue())
+def async_call_service():
+    gevent.spawn(call_service)
 
 @route('/')
 def index():
     """Home page"""
     title = "Image Processor App"
-    profile_service()
-    return template('index.tpl', data="Request completed!", title=title)
+    async_call_service()  # 非同期で呼び出し
+    return template('index.tpl', data="Request submitted!", title=title)
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=8000, debug=True, reloader=True)
-
+    run(host='0.0.0.0', port=8000, debug=True, reloader=True, server='gevent')
+    
 app = bottle.default_app()
