b5297b0df4c0aa325ca1a9e78233eb1bb150870b
toyohito.sawada <nqdior@gmail.com>
2024-06-28 18:43:39 +0900

    プロファイラの追加

--- index.py
+++ index.py
@@ -1,20 +1,36 @@
+import cProfile
+import pstats
+import io
 import bottle
-from bottle import route, run, Response, template
-import json
+from bottle import route, run, template
 import image
+from memory_profiler import profile
 
+@profile
 def call_service():
     directoryName = 'photos'
     image.process(directoryName)
+
+def profile_service():
+    pr = cProfile.Profile()
+    pr.enable()
+    call_service()
+    pr.disable()
+
+    s = io.StringIO()
+    sortby = 'cumulative'
+    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+    ps.print_stats()
+    print(s.getvalue())
 
 @route('/')
 def index():
     """Home page"""
     title = "Image Processor App"
-    call_service()
-    return template('index.tpl',data="Request completed!", title=title)
+    profile_service()
+    return template('index.tpl', data="Request completed!", title=title)
 
 if __name__ == '__main__':
-	run(host='0.0.0.0', port=8000, debug=True, reloader=True)
-	
+    run(host='0.0.0.0', port=8000, debug=True, reloader=True)
+
 app = bottle.default_app()
